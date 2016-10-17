b0dbb9ce9ae49fa3dfbc26abcfbe4a554f4b225f
w2820859 <617516207@qq.com>
2016-10-17 14:43:32 +0800

    89.10

--- 89.10.cpp
+++ 89.10.cpp
@@ -1,37 +1,51 @@
-  if(testSuite == "UehUeSCellEvalBasicTestSuiteD")
-  {
-    return new UehUeSCellEvalBasicTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalTwoScellSelectionPolicyTestSuiteD")
-  {
-    return new UehUeSCellEvalTwoScellSelectionPolicyTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalBasicScellSelectionPolicyTestSuiteD")
-  {
-    return new UehUeSCellEvalBasicScellSelectionPolicyTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalScellSelectionPolicyTDDTestSuiteD")
-  {
-    return new UehUeSCellEvalScellSelectionPolicyTDDTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalDynamicSingleScellSelectionPolicyTestSuiteD")
-  {
-    return new UehUeSCellEvalDynamicSingleScellSelectionPolicyTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalScellSelectionPolicyFDDTDDMixTestSuiteD")
-  {
-    return new UehUeSCellEvalScellSelectionPolicyFDDTDDMixTestSuiteD;
- }
-  else if (testSuite == "UehUeSCellEvalScellSelectionWithReducedMeasurementPolicyTestSuiteD")
+if(testSuite == "UehUeSCellEvalBasicTestSuiteD")
+{
+  return new UehUeSCellEvalBasicTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalTwoScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalTwoScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalBasicScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalBasicScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalScellSelectionPolicyTDDTestSuiteD")
+{
+  return new UehUeSCellEvalScellSelectionPolicyTDDTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalDynamicSingleScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalDynamicSingleScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalScellSelectionPolicyFDDTDDMixTestSuiteD")
+{
+  return new UehUeSCellEvalScellSelectionPolicyFDDTDDMixTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalScellSelectionWithReducedMeasurementPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalScellSelectionWithReducedMeasurementPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalUplinkScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalUplinkScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalThreeScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalThreeScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalFourScellSelectionPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalFourScellSelectionPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalDynamicSingleScellSelectionWithReducedMeasurementPolicyTestSuiteD")
+{
+  return new UehUeSCellEvalDynamicSingleScellSelectionWithReducedMeasurementPolicyTestSuiteD;
+}
+else if (testSuite == "UehUeSCellEvalTwoScellSelectionPolicyTDDTestSuiteD")
+{
+  return new UehUeSCellEvalTwoScellSelectionPolicyTDDTestSuiteD;
+}
  
-  {
-    return new UehUeSCellEvalScellSelectionWithReducedMeasurementPolicyTestSuiteD;
-  }
-  else if (testSuite == "UehUeSCellEvalScellSelectionPolicyTDDFDDMixTestSuiteD")
-  {
-    return new UehUeSCellEvalScellSelectionPolicyTDDFDDMixTestSuiteD;
-  }
- 
- 
-  TRACE_ERROR(STR("Test suite %s doesn't exist in testSuite().", (char*)testSuite));
-  return NULL;
+TRACE_ERROR(STR("Test suite %s doesn't exist in testSuite().", (char*)testSuite));
+return NULL;
